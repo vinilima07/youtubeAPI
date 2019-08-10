cf28cce004ccaa9372029ea56fcee184ee64a1e3
Vinicius Franca <vinilima07@gmail.com>
2019-08-10 10:00:39 -0300

    Up correct files working

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+//const CLIENT_ID = {CLIENT_ID};
+//const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+const API_KEY = 'AIzaSyAONVephJiOij7ryDiKQ-_RZ6p6Wvi4sLQ';
+
 function start() {
-  // 2. Initialize the JavaScript client library.
+  console.log('inicializando');
   gapi.client.init({
-    'apiKey': 'AIzaSyDPMc7rarDn1pgZCiRM-w6j_p1PcdH20JI',
-    // clientId and scope are optional if auth is not required.
-    'clientId': '269881579810-t5ovj6nm8ir2i7iokjo2glr8s79da57b.apps.googleusercontent.com',
-    'scope': 'https://www.googleapis.com/auth/youtube',
-  }).then(function() {
-    // 3. Initialize and make the API request.
-    return gapi.client.request({
-      'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
-    })
-  }).then(function(response) {
-    console.log(response.result);
-  }, function(reason) {
-    console.log('Error: ' + reason.result.error.message);
+    apiKey: 'AIzaSyAONVephJiOij7ryDiKQ-_RZ6p6Wvi4sLQ',
+    discoveryDocs:DISCOVERY_DOCS
+    //clientId: CLIENT_ID,
+    //scope: SCOPES
+  }).then(function(){
+
+    console.log('inicializado');
+    console.log('executando');
+    return gapi.client.youtube.playlistItems.list({
+      playlistId: 'PLmSkFzJxyNCIi21LboIt_SblqXuvnin9F',
+      part: 'snippet',
+      maxResults: 50 
+    });
+
+  }).then(function(response){
+      console.log('sucessifuly');
+      console.log(response);
+      console.log(response.result.items[0].snippet.title);
+  }, function(reason){
+      console.log('error'+reason);
   });
-};
+
+}
+
 // 1. Load the JavaScript client library.
 gapi.load('client', start);
